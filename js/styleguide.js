2f4934b2cc9708a47b074dd350da9541909d033e
Karissa Demi <k@karissademi.com>
2015-09-26 14:05:14 -0400

    Smoother pattern menu

--- js/styleguide.js
+++ js/styleguide.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $('body').scrollspy({
-    target: 'nav'
-  })
+    target: 'nav.main',
+    offset: 70
+  });
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 40) {
+      $('nav.main').removeClass('top');
+    } else {
+      $('nav.main').addClass('top');
+    }
+  });
+
 });
